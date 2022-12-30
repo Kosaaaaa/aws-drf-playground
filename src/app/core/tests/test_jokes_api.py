483a9277a3d72dfdd978c539b56de798558d4555
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-12-30 19:28:35 +0100

    test with custom exception handler

--- src/app/core/tests/test_jokes_api.py
+++ src/app/core/tests/test_jokes_api.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from core.utils import sub_test
 from core.views import JOKES_CATEGORIES, JOKES_LANGUAGES
 
 
@@ -36,6 +37,15 @@ class JokesApiTestCase(APITestCase):
 
         self.assertEqual(response.json()['jokes'], jokes)
 
-    def test_with_invalid_params(self):
-        response = self.client.get(add_url_query_params(self.url, {'language': 'p0p0p0', 'category': 'qwerty'}))
+    @sub_test([
+        {'test_url': add_url_query_params(url, {'language': 'p0p0p0', 'category': 'qwerty'})},
+        {'test_url': add_url_query_params(url, {'language': 'de', 'category': 'qwerty'})},
+        {'test_url': add_url_query_params(url, {'language': 'p0p0p0', 'category': 'twister'})},
+        {'test_url': add_url_query_params(url, {'category': 'qwerty'})},
+        {'test_url': add_url_query_params(url, {'language': 'qwerty'})},
+    ])
+    def test_with_invalid_params(self, test_url):
+        response = self.client.get(test_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('error', response.json())
+        self.assertTrue(response.json()['error'])
