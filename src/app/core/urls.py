4cb15b08133366a0d4fb60affc7af44cc743cc90
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-12-30 18:33:26 +0100

    fix tests validator in pre-commit config

--- src/app/core/urls.py
+++ src/app/core/urls.py
@@ -4,7 +4,7 @@ URL mappings for the core API.
 from core import views
 from django.urls import path
 
-app_name = 'user'
+app_name = 'core'
 
 urlpatterns = [
     path('jokes/', views.GetJokes.as_view(), name='jokes'),
